03b1faff84f4fbe4a45155e9addf4570e91b6d7b
B. O'Toole <botoole518@gmail.com>
2015-04-11 17:56:39 -0400

    added bootstrap, removed zurb

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
     <footer></footer>
-   
-    <script src="http://connect.soundcloud.com/sdk.js"></script>
+    <!--JS-->
+    <script src="//connect.soundcloud.com/sdk.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <script src="assets/js/vendor/isotope.min.js"></script>
     <script src="assets/js/vendor/colorbox.min.js"></script>
     <script src="assets/js/main.js"></script>
-   
  
     </body>
 </html>
